09300cc10fd4e7b5d37e2b7f5a94e1144cc6bad5
HenrikPLind <hpl17@student.aau.dk>
2024-09-15 13:16:45 +0200

    preprocessing_pipeline.py

--- preprocessing/preprocessing_pipeline.py
+++ preprocessing/preprocessing_pipeline.py
@@ -1,17 +1,36 @@
 import numpy as np
 import tifffile as tiff
 from patchify import patchify
+from itertools import product
+from PIL import Image
+def patchMaking(images, d, e, overlap):
+    patches = []
+    for image in images:
+        w, h = image.size
+        grid = list(product(range(0, h - h % d, overlap), range(0, w - w % e, overlap)))
+        for i, j in grid:
+            box = (j, i, j + d, i + e)
+            patch = image.crop(box)
+            patches.append(patch)
+    return patches
+
+def savePatches(patches, path, images, startImgNo):
+    filename = list()
+    for x in range(len(images)):  # Antallet af originale eller output billeder
+        for y in range(len(patches) // len(images)):  # Antallet af splits pr billede
+            name = "{0}{1}{2}{3}.png".format("image", x + 1 + startImgNo, "split", y + 1)
+            # filename = 'image ' + '%06d' + ' split' + ' %06d' % x, y  # image1 split1
+            filename.append(name)
+    i = 0
+    for patch in patches:
+        patch.save(path + filename[i])
+        i = i + 1
 
 def preprocessing(images):
-    for i in range(len(images)):
-        current_image = images[i]
-        # Normalize images
-        current_image = current_image/255
+    patches = patchMaking(images, d=256, e=256, overlap=128)
 
-        # patchify image
+    savePatches(patches=patches, path='', images=images, startImgNo=0)
 
-        patches = patchify(current_image, (256,256), step=256)
-
-        return patches
+    return patches
 
 
